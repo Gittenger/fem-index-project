2aba7bedf0d5922e1ba5f049db5275ea44d89756
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-19 15:23:46 -0400

    basic app setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from 'react'
+import styles from './App.module.scss'
+import './styles/main.scss'
+import data from './data.js'
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={styles.App}>
+			<div>
+				<header>
+					<p>
+						This is a collection of small but effective front-end projects geared
+						towards improving and showcasing my front-end skills.
+					</p>
+				</header>
+				<main>
+					<ol>
+						{data.map((item, i) => (
+							<li>
+								<span>{`${(i + 1).toString()}) `}</span>
+								<a href={item.link}>{item.name}</a>
+							</li>
+						))}
+					</ol>
+				</main>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
