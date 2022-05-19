34d5826cab7033bfd0b82f375e1970656c07a8fb
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-19 16:29:40 -0400

    header started

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react'
 import styles from './App.module.scss'
 import './styles/main.scss'
 import data from './data.js'
+import images from './assets/img-index.js'
 
 function App() {
+	const { Github } = images
+
 	return (
 		<div>
+			<header className={styles.header}>
+				<Github />
+			</header>
 			<div className={styles.mainBox}>
 				<div>
-				<p>
-					This is a collection of small but effective front-end projects geared
-					towards improving and showcasing my front-end skills.
-				</p>
+					<p>
+						This is a collection of small but effective front-end projects geared
+						towards improving and showcasing my front-end skills.
+					</p>
 					<p>
 						Each of these is a project based on a design provided by the{' '}
 						<a href="https://www.frontendmentor.io/">Frontend Mentor</a> website. This
@@ -20,16 +26,16 @@ function App() {
 						themselves with the design process.
 					</p>
 				</div>
-			<main>
-				<ol>
-					{data.map((item, i) => (
+				<main>
+					<ol>
+						{data.map((item, i) => (
 							<li key={i}>
-							<span>{`${(i + 1).toString()}) `}</span>
-							<a href={item.link}>{item.name}</a>
-						</li>
-					))}
-				</ol>
-			</main>
+								<span>{`${(i + 1).toString()}) `}</span>
+								<a href={item.link}>{item.name}</a>
+							</li>
+						))}
+					</ol>
+				</main>
 			</div>
 		</div>
 	)
