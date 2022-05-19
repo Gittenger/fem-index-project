4112bca1a6e21bc69cd985725986965ed8d2b6dc
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-19 15:44:20 -0400

    rm surrounding div

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import data from './data.js'
 function App() {
 	return (
 		<div className={styles.App}>
-			<div>
-				<header>
-					<p>
-						This is a collection of small but effective front-end projects geared
-						towards improving and showcasing my front-end skills.
-					</p>
-				</header>
-				<main>
-					<ol>
-						{data.map((item, i) => (
-							<li>
-								<span>{`${(i + 1).toString()}) `}</span>
-								<a href={item.link}>{item.name}</a>
-							</li>
-						))}
-					</ol>
-				</main>
-			</div>
+			<header>
+				<p>
+					This is a collection of small but effective front-end projects geared
+					towards improving and showcasing my front-end skills.
+				</p>
+			</header>
+			<main>
+				<ol>
+					{data.map((item, i) => (
+						<li>
+							<span>{`${(i + 1).toString()}) `}</span>
+							<a href={item.link}>{item.name}</a>
+						</li>
+					))}
+				</ol>
+			</main>
 		</div>
 	)
 }
