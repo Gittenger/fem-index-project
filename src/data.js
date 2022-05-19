21473552810f025602d65311e52b60cb17b70b98
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-19 15:23:36 -0400

    app data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = [
+	{
+		name: 'Social Media Dashboard',
+		link: 'https://gittenger.github.io/fem-social-media-dashboard/',
+	},
+	{
+		name: 'Easybank Landing Page',
+		link: 'https://gittenger.github.io/fem-easybank-landing/',
+	},
+	{
+		name: 'Sunnyside Agency Landing Page',
+		link: 'https://gittenger.github.io/fem-sunnyside-agency-landing/',
+	},
+	{
+		name: 'Blogr Landing Page',
+		link: 'https://gittenger.github.io/fem-blogr-landing-page/',
+	},
+	{
+		name: 'NFT Preview Card Component',
+		link: 'https://gittenger.github.io/fem-nft-preview-card-component/',
+	},
+	{
+		name: 'Project Intro Component',
+		link: 'https://gittenger.github.io/fem-project-intro-component/',
+	},
+	{
+		name: 'Insure Landing Page',
+		link: 'https://gittenger.github.io/fem-insure-landing-page/',
+	},
+	{
+		name: 'Bookmark Manager Landing',
+		link: 'https://gittenger.github.io/fem-bookmark-manager/',
+	},
+]
+
+export default data
