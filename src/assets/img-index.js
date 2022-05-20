e2a20964933e543db722077ed136e3625ecdf493
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-20 13:17:58 -0400

    linkedin and website icon

--- src/assets/img-index.js
+++ src/assets/img-index.js
@@ -1,9 +1,13 @@
 import react from 'react'
 
 import { ReactComponent as Github } from './github.svg'
+import { ReactComponent as LinkedIn } from './linkedin.svg'
+import { ReactComponent as WebIcon } from './sphere.svg'
 
 const images = {
 	Github,
+	LinkedIn,
+	WebIcon,
 }
 
 export default images
